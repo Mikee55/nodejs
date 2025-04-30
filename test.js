27ac73e1a528ecb7b296f88dcf4f4e610a3264ed
Mikee55 <milkyasterefe@gmail.com>
2025-04-30 22:13:56 +0300

    http

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-function greet(callBack) {
-  console.log("Halo");
-  var data = "Mike";
-  callBack(data);
-}
+// const http = require("http");
 
-greet(function (data) {
-  console.log(`first Callback ${data}`);
-});
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { contenttype: "text-plain" });
+//     res.end("Hello Mike!");
+//   })
+
+//   .listen(1337, "127.0.0.1");
+
+console.log("my");
