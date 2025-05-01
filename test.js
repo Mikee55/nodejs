8634a0bdf5c16c94a18e51a0f93df86f39d9dbb3
Mikee55 <milkyasterefe@gmail.com>
2025-05-01 03:47:06 +0300

    Server

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-// const http = require("http");
+const http = require("http");
+const fs = require("fs");
 
-// http
-//   .createServer(function (req, res) {
-//     res.writeHead(200, { contenttype: "text-plain" });
-//     res.end("Hello Mike!");
-//   })
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { contenttype: "text/html" });
+    let html = fs.readFileSync(__dirname + "/index.html");
+    res.end(html);
+  })
 
-//   .listen(1337, "127.0.0.1");
-
-console.log("my");
+  .listen(1337, "127.0.0.1");
